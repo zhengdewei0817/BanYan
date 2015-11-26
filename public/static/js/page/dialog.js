bdcec4eb4836fe30c5b61376a1ed23e1b8657ff9
lidongyue <dong_yuehappy@sina.com>
2015-11-26 16:42:24 +0800

    dialog test06

---
+++ public/static/js/page/dialog.js
@@ -0,0 +1,7 @@
+import Dialog from '../component/Dialog';
+import '../../less/dialog/dialog.less';
+
+new Dialog().setConfig({
+    title: '提示信息',
+    content: `<i></i><h2>fgf</h2><p>fsfsafsfsafsafsafds</p>`
+}).init();
